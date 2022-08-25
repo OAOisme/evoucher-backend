36402fc035332b0ec541020919738b106c6adf96
Ojo Abiola Emmanuel <oao@Ojos-MacBook-Air.local>
2022-08-25 12:13:48 +0100

    one

--- index.js
+++ index.js
@@ -20,4 +20,4 @@ app.use('/', main)
 
 
 
-app.listen(process.env.port || 3000, () => console.log('Listening on port 3000...'))
+app.listen(process.env.PORT || 3000, () => console.log('Listening on port 3000...'))
